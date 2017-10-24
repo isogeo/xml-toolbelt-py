c6a6b7fbcc97d2f915d30c1b4a75073bfbe55ca8
GeoJulien <julien.moura+dev@gmail.com>
2017-10-24 23:39:35 +0200

    method to automatically set dataset creation date

--- md_xml_fixer.py
+++ md_xml_fixer.py
@@ -2,6 +2,7 @@
 #! /usr/bin/python
 # -*- coding: UTF-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
+
 # -----------------------------------------------------------------------------
 # Name:         Metadata XML fixer
 # Purpose:      Check
@@ -18,20 +19,41 @@ from __future__ import (absolute_import, print_function, unicode_literals)
 from datetime import datetime, date
 from copy import deepcopy               # advanced copy
 import logging
+from logging.handlers import RotatingFileHandler
 from os import getcwd, listdir, mkdir, path
 import sys
+from xml.dom import minidom
+from xml.etree import ElementTree as ET
 
 # Python 3 backported
 from collections import OrderedDict
 
-# 3rd party libraries
-from xml.etree import ElementTree as ET
+# imports depending on Python version
+if (sys.version_info < (3, 0)):
+    from io import open
+else:
+    pass
+
 
 # ##############################################################################
 # ############ Globals ############
 # #################################
 
-ds_creation_date = ""
+# LOG
+logger = logging.getLogger("XML_ISO19139_FIXER")
+logging.captureWarnings(True)
+logger.setLevel(logging.DEBUG)
+log_form = logging.Formatter("%(asctime)s || %(levelname)s "
+                             "|| %(module)s || %(lineno)s || %(message)s")
+logfile = RotatingFileHandler("LOG_XML_FIXER.log", "a", 5000000, 1)
+logfile.setLevel(logging.DEBUG)
+logfile.setFormatter(log_form)
+logger.addHandler(logfile)
+logger.info("Python version: {}"
+            .format(sys.version_info))
+
+# customize script
+ds_creation_date = "2017-09-08"
 
 # #############################################################################
 # ########### Classes #############
@@ -39,6 +61,8 @@ ds_creation_date = ""
 
 
 class MetadataXML19139Fixer(object):
+    """ISO 19139 XML fixer."""
+
     def __init__(self):
         """Batch edit input XML files to enhance compliance to ISO19139."""
         self.check_folders()
@@ -52,32 +76,32 @@ class MetadataXML19139Fixer(object):
         # input folder
         if not path.isdir(self.fold_in):
             try:
-                mkdir(self.fold_in, "0777")
+                mkdir(self.fold_in, "0o777")
             except Exception as e:
-                logging.error(e)
+                logger.error(e)
                 sys.exit()
         else:
-            logging.info("Input folder already exists.")
+            logger.info("Input folder already exists.")
             pass
 
         # input XML files
         if not len(listdir(self.fold_in)):
-            logging.error("Input folder was not created, so "
-                          "there is not any XML file. Please "
-                          "copy your XML ISO19139 files inside.")
+            logger.error("Input folder was not created, so "
+                         "there is not any XML file. Please "
+                         "copy your XML ISO19139 files inside.")
             sys.exit()
         else:
-            logging.info("Files are present in input folder.")
+            logger.info("Files are present in input folder.")
 
         # output folder
         if not path.isdir(self.fold_out):
             try:
-                mkdir(self.fold_out, "0777")
+                mkdir(self.fold_out, "0o777")
             except Exception as e:
-                logging.error(e)
+                logger.error(e)
                 sys.exit()
         else:
-            logging.info("Output folder already exists.")
+            logger.info("Output folder already exists.")
             pass
         # method ending
         return 0
@@ -90,8 +114,15 @@ class MetadataXML19139Fixer(object):
               "gco": "http://www.isotc211.org/2005/gco",
               "gmd": "http://www.isotc211.org/2005/gmd",
               "gmx": "http://www.isotc211.org/2005/gmx",
-              "srv": "http://www.isotc211.org/2005/srv"}
+              "srv": "http://www.isotc211.org/2005/srv",
+              "xl": "http://www.w3.org/1999/xlink"}
+
+        # register namespaces
+        for namespace in ns:
+            ET.register_namespace(namespace, ns.get(namespace))
         return ns
+
+    # -------- Methods to add missing XML parts --------------------------------------
 
     def add_ds_creation_date(self):
         """Add metadata creation date into metadata XML.
@@ -108,45 +139,93 @@ class MetadataXML19139Fixer(object):
             </CI_Date>
         </date>
         """
-        for item in self.get_identificationInfo().find('gmd:MD_DataIdentification', self.ns):
-            print(item)
+        # date = ET.SubElement(elem, "<gmd:date>")
+        ci_citation = self.get_md_ci_citation()
+        # quicky way - NOT WORKING :'(
+        # in_date = '''
+        # <gmd:date gco="http://www.isotc211.org/2005/gco" gmd="http://www.isotc211.org/2005/gmd">
+        #     <gmd:CI_Date>
+        #         <gmd:date>
+        #             <gco:Date>{}Z</gco:Date>
+        #         </gmd:date>
+        #         <gmd:dateType>
+        #             <gmd:CI_DateTypeCode codeList="http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/ML_gmxCodelists.xml#CI_DateTypeCode" codeListValue="creation">creation</gmd:CI_DateTypeCode>
+        #         </gmd:dateType>
+        #     </gmd:CI_Date>
+        # </gmd:date>
+        # '''.format(ds_creation_date)
+        # ET.SubElement(ci_citation, in_date)
 
-        return
 
-    def get_identificationInfo(self):
+        # fastidious way - WORKING
+        parent_date = ET.SubElement(ci_citation, "gmd:date")
+        ci_date = ET.SubElement(parent_date, "gmd:CI_date")
+        sub_date = ET.SubElement(ci_date, "gmd:date")
+        # date value
+        value_date = ET.SubElement(sub_date, "gco:date")
+        value_date.text = "{}Z".format(ds_creation_date)
+        # date type
+        sub_date_type = ET.SubElement(ci_date, "gmd:dateType")
+        sub_ci_date_typecode = ET.SubElement(sub_date_type, "gmd:CI_DateTypeCode")
+        sub_ci_date_typecode.set("codeList", "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/ML_gmxCodelists.xml#CI_DateTypeCode")
+        sub_ci_date_typecode.set("codeListValue", "creation")
+        sub_ci_date_typecode.text = "creation"
+
+    # -------- Methods to get XML parts --------------------------------------
+
+    def get_identification_info(self):
         """Get XML main first levels items."""
-        for i in self.tpl_root.findall('gmd:identificationInfo', self.ns):
+        for i in self.tpl_root.findall('gmd:identificationInfo',
+                                       self.ns):
             self.idenfo = i
 
         return self.idenfo
-        
+
+    def get_md_data_identification(self):
+        """Get MD_DataIdentification level items."""
+        return self.get_identification_info().find('gmd:MD_DataIdentification',
+                                                   self.ns)
+
+    def get_md_ci_citation(self):
+        """Get CI_Citation level items."""
+        pth_ci_citation = "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation"
+        return self.tpl_root.find(pth_ci_citation,
+                                  self.ns)
+
+    # -------- XML utilis ----------------------------------------------------
+
+    def prettify(self, elem):
+        """Return a pretty-printed XML string for the Element."""
+        rough_string = ET.tostring(elem, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="  ")
+
+    # -------- LEGACY ----------------------------------------------------
 
     def iso19139(self, dest, dico_layer, dico_profil, blabla):
-        u"""
-        Export to xml file according to the ISO 19139
-        """
+        u"""Export to xml file according to the ISO 19139."""
         # opening the template
         with open(r"data/xml/template_iso19139.xml", 'r')as iso:
             # parser
             template = ET.parse(iso)
             # namespaces
-            namespaces = ET.register_namespace("gts","http://www.isotc211.org/2005/gts")
-            namespaces = ET.register_namespace("gml","http://www.opengis.net/gml")
-            namespaces = ET.register_namespace("xsi","http://www.w3.org/2001/XMLSchema-instance")
-            namespaces = ET.register_namespace("gco","http://www.isotc211.org/2005/gco")
-            namespaces = ET.register_namespace("gmd","http://www.isotc211.org/2005/gmd")
-            namespaces = ET.register_namespace("gmx","http://www.isotc211.org/2005/gmx")
-            namespaces = ET.register_namespace("srv","http://www.isotc211.org/2005/srv")
+            namespaces = ET.register_namespace("gts", "http://www.isotc211.org/2005/gts")
+            namespaces = ET.register_namespace("gml", "http://www.opengis.net/gml")
+            namespaces = ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+            namespaces = ET.register_namespace("gco", "http://www.isotc211.org/2005/gco")
+            namespaces = ET.register_namespace("gmd", "http://www.isotc211.org/2005/gmd")
+            namespaces = ET.register_namespace("gmx", "http://www.isotc211.org/2005/gmx")
+            namespaces = ET.register_namespace("srv", "http://www.isotc211.org/2005/srv")
             # getting the elements and sub-elements structure
             tpl_root = template.getroot()
             # transform coordinates to WGS84 for catalogs display
             if dico_layer.get('EPSG') != u'None':
                 u""" if ogr found the ESPG code """
-                srs84 = Transproj(epsg = int(dico_layer.get('EPSG')),
-                                  Xmin = dico_layer.get('Xmin'),
-                                  Ymin = dico_layer.get('Ymin'),
-                                  Xmax = dico_layer.get('Xmax'),
-                                  Ymax = dico_layer.get('Ymax')).tupwgs84
+                srs84 = Transproj(epsg=int(dico_layer.get('EPSG')),
+                                  Xmin=dico_layer.get('Xmin'),
+                                  Ymin=dico_layer.get('Ymin'),
+                                  Xmax=dico_layer.get('Xmax'),
+                                  Ymax=dico_layer.get('Ymax')).tupwgs84
             else:
                 u""" if not... """
                 srs84 = (dico_layer.get('Xmin'),
@@ -321,10 +400,10 @@ if __name__ == '__main__':
     app = MetadataXML19139Fixer()
     print(dir(app))
     for xml in listdir(app.fold_in):
-        logging.info(xml)
+        logger.info(xml)
         print(xml)
         # opening the input
-        with open(path.join(app.fold_in, xml), 'r') as in_xml:
+        with open(path.join(app.fold_in, xml), 'r', encoding="utf-8") as in_xml:
             print(in_xml)
             # parser
             app.tpl = ET.parse(in_xml)
@@ -333,9 +412,12 @@ if __name__ == '__main__':
             # creation date
             app.add_ds_creation_date()
 
+            # # namespaces
+            # for ns in app.ns:
+            #     ET.register_namespace(ns, app.ns.get(ns))
             # saving the output xml file
             app.tpl.write(path.join(app.fold_out, xml),
-                           encoding='utf-8',
-                           xml_declaration='version="1.0"',
-                           # default_namespace=namespaces,
-                           method='xml')
+                          encoding='utf-8',
+                          xml_declaration=1,
+                          # default_namespace=app.ns.get("gmd"),
+                          method='xml')
