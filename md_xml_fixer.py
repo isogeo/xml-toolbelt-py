a50d7780ba6da06a808421c73949a1caa8f8a5dc
GeoJulien <julien.moura+dev@gmail.com>
2017-10-24 23:38:38 +0200

    precise Python version to use

--- md_xml_fixer.py
+++ md_xml_fixer.py
@@ -1,3 +1,5 @@
+#! python3
+#! /usr/bin/python
 # -*- coding: UTF-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
 # -----------------------------------------------------------------------------
