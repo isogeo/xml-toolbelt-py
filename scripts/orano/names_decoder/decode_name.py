342b96b0c2f93f16c60877819311f1ddd65d6981
Guts <Guts@users.noreply.github.com>
2019-09-03 12:06:49 +0200

    apply black to scripts

--- scripts/orano/names_decoder/decode_name.py
+++ scripts/orano/names_decoder/decode_name.py
@@ -15,39 +15,44 @@
 import xml.etree.ElementTree as ET
 import logging
 import csv
-import os.path 
+import os.path
 
 # #############################################################################
 # ######## Intialize log #################
 # ##################################
-logging.basicConfig(filename='log.log', format="%(asctime)s || %(funcName)s || %(levelname)s || %(message)s",
-                    level=logging.DEBUG)
+logging.basicConfig(
+    filename="log.log",
+    format="%(asctime)s || %(funcName)s || %(levelname)s || %(message)s",
+    level=logging.DEBUG,
+)
+
 
 def list_filenames_xml_from_directory(path: str) -> list:
-    
-# #############################################################################
-# ######## Extract XML filenames from a path #################
-# ##################################
 
-    list_filenames_xml=[]
-    counter = 0 
+    # #############################################################################
+    # ######## Extract XML filenames from a path #################
+    # ##################################
+
+    list_filenames_xml = []
+    counter = 0
     for root, dirs, files in os.walk(path):
         for i in files:
-            ext = os.path.splitext(i)[1] #get extension
-            if ext ==".xml": #get only xml files
-                counter+=1
-                name = i.split('.')[0] #get name without extension
+            ext = os.path.splitext(i)[1]  # get extension
+            if ext == ".xml":  # get only xml files
+                counter += 1
+                name = i.split(".")[0]  # get name without extension
                 list_filenames_xml.append({"name": name, "path": os.path.join(root, i)})
-                              
+
     logging.info("{} XML filenames founded in {}".format(counter, path))
 
     return list_filenames_xml
 
-def decode_name(name: str, config: str, path: str)-> dict:
 
-# #############################################################################
-# ######## Intialize variable #################
-# ##################################
+def decode_name(name: str, config: str, path: str) -> dict:
+
+    # #############################################################################
+    # ######## Intialize variable #################
+    # ##################################
 
     name_country = None
     name_region = None
@@ -57,19 +62,21 @@ def decode_name(name: str, config: str, path: str)-> dict:
     name_theme2 = None
     name_theme3 = None
 
-# #############################################################################
-# ######## Extract content from string name #################
-# ##################################
-    logging.info("Split name : {}".format(name)) 
+    # #############################################################################
+    # ######## Extract content from string name #################
+    # ##################################
+    logging.info("Split name : {}".format(name))
 
     code_country = name[0:2]
-    if code_country == "GA": #exception for Gabon GA_C__KAYA_XX_XXXX_XXXXX_____01 → Country = Gabon Region = C_KAYA_KAYA
+    if (
+        code_country == "GA"
+    ):  # exception for Gabon GA_C__KAYA_XX_XXXX_XXXXX_____01 → Country = Gabon Region = C_KAYA_KAYA
         code_region = name[3:10]
         code_maintheme = name[11:13]
-        try: 
-            #verify year format and date
+        try:
+            # verify year format and date
             number = int(name[14:18])
-            if (number >= 1900 and number <= 2100):
+            if number >= 1900 and number <= 2100:
                 year = number
                 logging.info("Year = {}".format(year))
         except:
@@ -78,13 +85,13 @@ def decode_name(name: str, config: str, path: str)-> dict:
         code_theme2 = name[25:27]
         code_theme3 = name[27:29]
         logging.warning("Gabon exception : Region's code will containt 6 characters. ")
-    else: 
-        code_region = name[3:7] 
+    else:
+        code_region = name[3:7]
         code_maintheme = name[8:10]
         try:
-            #verify year format and date
+            # verify year format and date
             number = int(name[11:15])
-            if (number >= 1900 and number <= 2100):
+            if number >= 1900 and number <= 2100:
                 year = number
                 logging.info("Year = {}".format(year))
         except:
@@ -93,9 +100,9 @@ def decode_name(name: str, config: str, path: str)-> dict:
         code_theme2 = name[22:24]
         code_theme3 = name[24:26]
 
-# #############################################################################
-# ######## Parse configuration XML #################
-# ##################################
+    # #############################################################################
+    # ######## Parse configuration XML #################
+    # ##################################
 
     tree = ET.parse(config)
     root = tree.getroot()
@@ -103,102 +110,121 @@ def decode_name(name: str, config: str, path: str)-> dict:
     mainthemes = root[1]
     scales = root[2]
 
-# #############################################################################
-# ######## Search Country and Region #################
-# ##################################
+    # #############################################################################
+    # ######## Search Country and Region #################
+    # ##################################
     logging.info("Search Country and Region...")
     for country in countries:
-        if code_country == country.get('code'):
-            name_country = country.get('desc')
-            regions = country[0]            
+        if code_country == country.get("code"):
+            name_country = country.get("desc")
+            regions = country[0]
             for region in regions:
-                if code_region == region.get('code'):
-                    name_region = region.get('desc')
+                if code_region == region.get("code"):
+                    name_region = region.get("desc")
                     break
             break
 
-    if name_country :
-        logging.info(
-            "Country = {}".format(name_country))
+    if name_country:
+        logging.info("Country = {}".format(name_country))
     else:
         logging.error("No country found")
 
-    if name_region :
+    if name_region:
         logging.info(
-        "Country {} an Region {} have been extracted.".format(name_country,name_region))
+            "Country {} an Region {} have been extracted.".format(
+                name_country, name_region
+            )
+        )
     else:
         logging.error("No region found")
 
-# #############################################################################
-# ######## Search Mainthemes and SubThemes #################
-# ##################################
+    # #############################################################################
+    # ######## Search Mainthemes and SubThemes #################
+    # ##################################
     logging.info("Search Main theme and Subtheme(s)...")
-    if code_country == "GA": #Gabon exception
+    if code_country == "GA":  # Gabon exception
         for maintheme in mainthemes:
-            if code_maintheme == maintheme.get('code'):
-                name_maintheme = maintheme.get('desc')
+            if code_maintheme == maintheme.get("code"):
+                name_maintheme = maintheme.get("desc")
                 logging.info("Main theme is :  {} ".format(name_maintheme))
                 for subtheme in maintheme:
-                    if code_theme2 == subtheme.get('code'): 
-                        name_theme2 = subtheme.get('desc')
-                        logging.info("Theme 2 is : {}".format(name_theme2))                     
-                    elif code_theme3 == subtheme.get('code'):
-                        name_theme3 = subtheme.get('desc')
-                    elif name[25:28] == subtheme.get('code'): #Subtheme exceptions (when theme 2 composed by 3 characters)
-                        logging.warning("Theme 2 containt 3 characters, so  there is no theme 3")
-                        name_theme2 = subtheme.get('desc')
+                    if code_theme2 == subtheme.get("code"):
+                        name_theme2 = subtheme.get("desc")
+                        logging.info("Theme 2 is : {}".format(name_theme2))
+                    elif code_theme3 == subtheme.get("code"):
+                        name_theme3 = subtheme.get("desc")
+                    elif name[25:28] == subtheme.get(
+                        "code"
+                    ):  # Subtheme exceptions (when theme 2 composed by 3 characters)
+                        logging.warning(
+                            "Theme 2 containt 3 characters, so  there is no theme 3"
+                        )
+                        name_theme2 = subtheme.get("desc")
                         code_theme2 = name[25:28]
-                    elif name[25:29] == subtheme.get('code'): #Subtheme exceptions (when theme 2 composed by 4 characters)
-                        logging.warning("Theme 2 containt 4 characters, so there is no theme 3")
-                        name_theme2 = subtheme.get('desc')
+                    elif name[25:29] == subtheme.get(
+                        "code"
+                    ):  # Subtheme exceptions (when theme 2 composed by 4 characters)
+                        logging.warning(
+                            "Theme 2 containt 4 characters, so there is no theme 3"
+                        )
+                        name_theme2 = subtheme.get("desc")
                         code_theme2 = name[25:29]
 
-    else: 
+    else:
         for maintheme in mainthemes:
-            if code_maintheme == maintheme.get('code'):
-                name_maintheme = maintheme.get('desc')
+            if code_maintheme == maintheme.get("code"):
+                name_maintheme = maintheme.get("desc")
                 for subtheme in maintheme:
-                    if code_theme2 == subtheme.get('code'): 
-                        name_theme2 = subtheme.get('desc')
-                    elif code_theme3 == subtheme.get('code'):
-                        name_theme3 = subtheme.get('desc')
-                    elif name[22:25] == subtheme.get('code'): #Subtheme exceptions (when theme 2 composed by 3 characters)
-                        logging.warning("Theme 2 containt 3 characters, so there is no theme 3")
-                        name_theme2 = subtheme.get('desc')
+                    if code_theme2 == subtheme.get("code"):
+                        name_theme2 = subtheme.get("desc")
+                    elif code_theme3 == subtheme.get("code"):
+                        name_theme3 = subtheme.get("desc")
+                    elif name[22:25] == subtheme.get(
+                        "code"
+                    ):  # Subtheme exceptions (when theme 2 composed by 3 characters)
+                        logging.warning(
+                            "Theme 2 containt 3 characters, so there is no theme 3"
+                        )
+                        name_theme2 = subtheme.get("desc")
                         code_theme2 = name[22:25]
-                    elif name[22:26] == subtheme.get('code'): #Subtheme exceptions (when theme 2 composed by 4 characters)
-                        logging.warning("Theme 2 containt 4 characters, so there is no theme 3")
-                        name_theme2 = subtheme.get('desc')
+                    elif name[22:26] == subtheme.get(
+                        "code"
+                    ):  # Subtheme exceptions (when theme 2 composed by 4 characters)
+                        logging.warning(
+                            "Theme 2 containt 4 characters, so there is no theme 3"
+                        )
+                        name_theme2 = subtheme.get("desc")
                         code_theme2 = name[22:26]
 
     if name_maintheme:
-        logging.info(
-            "Main theme = {}".format(name_maintheme))
+        logging.info("Main theme = {}".format(name_maintheme))
     else:
         logging.error("No Main theme found")
 
-    if name_region :
+    if name_region:
         logging.info(
-            "Main theme is {} and Subtheme(s) are {} and {} ".format(name_maintheme,name_theme2,name_theme3))
+            "Main theme is {} and Subtheme(s) are {} and {} ".format(
+                name_maintheme, name_theme2, name_theme3
+            )
+        )
     else:
         logging.error("No Theme 2 found")
-                      
 
-# #############################################################################
-# ######## Search scale #################
-# ##################################
-    logging.info("Search Scale...")                    
+    # #############################################################################
+    # ######## Search scale #################
+    # ##################################
+    logging.info("Search Scale...")
     for scale in scales:
-        if code_scale == scale.get('code'):
-            name_scale = scale.get('desc')
-    
+        if code_scale == scale.get("code"):
+            name_scale = scale.get("desc")
+
     if name_scale:
         logging.info("Scale is {} ".format(name_scale))
     else:
         logging.error("No Scale found")
-    
+
     result = {
-        "Name" : name,
+        "Name": name,
         "Country": name_country,
         "Region": name_region,
         "Main Theme": name_maintheme,
@@ -206,12 +232,13 @@ def decode_name(name: str, config: str, path: str)-> dict:
         "Scale": name_scale,
         "Theme 2": name_theme2,
         "Theme 3": name_theme3,
-        "Path": path
-        }
-    
+        "Path": path,
+    }
+
     logging.info("Extraction finished, result is {}".format(result))
-    
+
     return result
+
 
 # #############################################################################
 # ##### Stand alone program ########
@@ -220,32 +247,54 @@ def decode_name(name: str, config: str, path: str)-> dict:
 if __name__ == "__main__":
 
     config = "axinite.xml"
-    path =  "\\Users\LéoDARENGOSSE\ISOGEO\SIG - Documents\CLIENTS\85_ORANO\Echantillon"
+    path = "\\Users\LéoDARENGOSSE\ISOGEO\SIG - Documents\CLIENTS\85_ORANO\Echantillon"
 
-    #write csv with decodage result
-    with open('result.csv', 'w', newline='') as csvfile:
-        fieldnames = ["Name","Country","Region","Main Theme","Year","Scale","Theme 2","Theme 3", "Path"]
-        
-        csv.register_dialect('semicolon', delimiter=';') #create dialect
+    # write csv with decodage result
+    with open("result.csv", "w", newline="") as csvfile:
+        fieldnames = [
+            "Name",
+            "Country",
+            "Region",
+            "Main Theme",
+            "Year",
+            "Scale",
+            "Theme 2",
+            "Theme 3",
+            "Path",
+        ]
 
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='semicolon') #write csv with a dictionnary
-        writer.writeheader() #write header
+        csv.register_dialect("semicolon", delimiter=";")  # create dialect
 
-        list_names_path = list_filenames_xml_from_directory(path) #list names
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, dialect="semicolon"
+        )  # write csv with a dictionnary
+        writer.writeheader()  # write header
+
+        list_names_path = list_filenames_xml_from_directory(path)  # list names
 
         counter_csv = 0
         counter_undecodable = 0
         for dict_name_path in list_names_path:
             name = dict_name_path.get("name")
-            path = dict_name_path.get("path")        
+            path = dict_name_path.get("path")
             result = decode_name(name, config, path)
             country = result.get("Country")
-            if country is None : #if the country can't be found, the name might be undecodable
-                logging.error("Name {} might be undecodable and will no be added to CSV Result".format(result.get("Name")))
-                counter_undecodable+=1
+            if (
+                country is None
+            ):  # if the country can't be found, the name might be undecodable
+                logging.error(
+                    "Name {} might be undecodable and will no be added to CSV Result".format(
+                        result.get("Name")
+                    )
+                )
+                counter_undecodable += 1
             else:
-                writer.writerow(result) #add result dictionary to csv
+                writer.writerow(result)  # add result dictionary to csv
                 logging.info("Add {} to CSV Result".format(result.get("Name")))
-                counter_csv+=1
+                counter_csv += 1
 
-    logging.info("{} filenames have been added to CSV Result, {} not.".format(counter_csv, counter_undecodable))
+    logging.info(
+        "{} filenames have been added to CSV Result, {} not.".format(
+            counter_csv, counter_undecodable
+        )
+    )
